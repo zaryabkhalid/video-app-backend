4342cea10c4a963b03a27853aa467cdbaea30186
Zaryab khalid <zaryabkhalid43@gmail.com>
2024-01-07 00:16:05 +0500

    Add comment, like, playlist, and tweet models

---
+++ src/models/comment.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+import mongooseAggregate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    videos: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+commentSchema.plugin(mongooseAggregate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
