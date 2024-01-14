982c21e565f538fa056c82c9f5b2ea7d8ed53836
Zaryab khalid <zaryabkhalid43@gmail.com>
2024-01-14 18:55:43 +0500

    feature: upload a video

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -18,10 +18,60 @@ const getAllVideos = expressAsyncHandler(async (req, res) => {
  */
 const publishAVideo = expressAsyncHandler(async (req, res) => {
   // TODO: Get upload video on a cloudinary and save its link on db.
+
+  const { title, description } = req.body;
+
+  if ([title, description].some((field) => field?.trim() === "")) {
+    throw new ApiError("Title or Description is required");
+  }
+
+  let videoFileLocalPath;
+  let thumbnailLocalPath;
+
+  // Verifying VideoFilePath
+  if (req.files && Array.isArray(req.files.videoFile) && req.files.videoFile.length > 0) {
+    videoFileLocalPath = req.files.videoFile[0].path;
+  }
+
+  if (!videoFileLocalPath) {
+    throw new ApiError("Video File is required");
+  }
+
+  // Verifying Thumbnail Path
+
+  if (req.files && Array.isArray(req.files.thumbnail) && req.files.thumbnail.length > 0) {
+    thumbnailLocalPath = req.files.thumbnail[0].path;
+  }
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError("Thumbnail is required");
+  }
+
+  const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+  console.log("VideoFile: ", videoFile);
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+  console.log("Thumbnail: ", thumbnail);
+
+  const savedVideo = await Video.create({
+    title,
+    description,
+    videoFile: videoFile?.url,
+    duration: videoFile?.duration,
+    thumbnail: thumbnail?.url,
+    owner: req.user._id,
+  });
+
+  if (!savedVideo) {
+    throw new ApiError(500, "Something went wrong");
+  }
+
+  return res.status(201).json(new ApiResponse(200, savedVideo, "Video Uploaded Successfully..."));
 });
 
 /**
+ * ----------------------
  * @method GetVideoById
+ * ----------------------
  */
 const getVideoById = expressAsyncHandler(async (req, res) => {
   // TODO: get video by using video ID
