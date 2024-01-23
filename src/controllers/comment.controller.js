b6e4de69033fe9ce354ee2341c828653904f9436
Zaryab khalid <zaryabkhalid43@gmail.com>
2024-01-23 23:57:02 +0500

    comment create update delete feature implemented.

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -21,6 +21,31 @@ const getVideoComments = expressAsyncHandler(async (req, res) => {
 
 const addComment = expressAsyncHandler(async (req, res) => {
   // TODO: Add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+  const userId = req.user._id;
+
+  const isValidId = isValidObjectId(new mongoose.Types.ObjectId(videoId));
+
+  if (!isValidId) {
+    throw new ApiError(400, "video Id isn't valid...");
+  }
+
+  if (!content) {
+    throw new ApiError(400, "content is required");
+  }
+
+  const newContent = await Comment.create({
+    content: content,
+    videos: videoId,
+    owner: userId,
+  });
+
+  if (!newContent) {
+    throw new ApiError(500, "Something went wrong while commenting please try again later...");
+  }
+
+  return res.status(201).json(new ApiResponse(201, newContent, "Successfully commented on Video"));
 });
 
 /**
@@ -29,6 +54,36 @@ const addComment = expressAsyncHandler(async (req, res) => {
 
 const updateComment = expressAsyncHandler(async (req, res) => {
   // TODO: Update a Comment on a video
+
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!commentId) {
+    throw new ApiError(400, "Comment Id is not valid...");
+  }
+
+  if (!content) {
+    throw new ApiError(400, "Content is required...");
+  }
+
+  const filteredComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content: content,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!filteredComment) {
+    throw new ApiError(402, "something Went Wrong");
+  }
+  console.log("Comment find by using userId", filteredComment);
+
+  return res.status(200).json(new ApiResponse(200, filteredComment, "comment update successfully..."));
 });
 
 /**
@@ -37,6 +92,22 @@ const updateComment = expressAsyncHandler(async (req, res) => {
 
 const deleteComment = expressAsyncHandler(async (req, res) => {
   // TODO: Delete a comment from a vedio
+
+  const { commentId } = req.params;
+
+  if (!commentId) {
+    throw new ApiError(400, "comment id is required");
+  }
+
+  const commentToBeDeleted = await Comment.findByIdAndDelete(commentId);
+
+  if (!commentToBeDeleted) {
+    throw new ApiError(400, "Something went wrong while deleting comment.");
+  }
+
+  const deleteCommentId = commentToBeDeleted._id;
+
+  return res.status(200).json(new ApiResponse(200, { deleteCommentId }, "comment deletd successfully..."));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
