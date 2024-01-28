12ff678c3509f408b99cc581400781938e8bea11
Zaryab khalid <zaryabkhalid43@gmail.com>
2024-01-28 21:09:51 +0500

    Feature: Dashboard get stats implemented

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -12,6 +12,71 @@ import { expressAsyncHandler } from "../utils/expressAsyncHandler.js";
 
 const getChannelStats = expressAsyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscriber, total videos, total likes etc.
+
+  const statAggregation = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req?.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likes: {
+          $size: "$likes",
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalViews: {
+          $sum: "$views",
+        },
+        totalVideos: {
+          $sum: 1,
+        },
+        totalLikes: {
+          $sum: "$likes",
+        },
+      },
+    },
+    {
+      $addFields: {
+        owner: new mongoose.Types.ObjectId(req?.user_id),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "owner",
+        foreignField: "channel",
+        as: "totalSubscriber",
+      },
+    },
+    {
+      $addFields: {
+        totalSubscriber: {
+          $size: "$totalSubscriber",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        owner: 0,
+      },
+    },
+  ]);
+
+  return res.status(200).json(new ApiResponse(200, statAggregation, "Stats get successfully"));
 });
 
 /**
@@ -20,6 +85,54 @@ const getChannelStats = expressAsyncHandler(async (req, res) => {
 
 const getChannelVideos = expressAsyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded bt the channel
+
+  const userId = req.user._id;
+
+  const uploadedVideosByChannel = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "created_By",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        isPublished: 0,
+        owner: 0,
+      },
+    },
+    {
+      $addFields: {
+        created_By: {
+          $arrayElemAt: ["$created_By", 0],
+        },
+      },
+    },
+  ]);
+
+  if (!uploadedVideosByChannel || uploadedVideosByChannel.length <= 0) {
+    throw new ApiError(400, "Video list is Empty");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, uploadedVideosByChannel, "Successfully get all videos uploaded by channel"));
 });
 
 export { getChannelStats, getChannelVideos };
