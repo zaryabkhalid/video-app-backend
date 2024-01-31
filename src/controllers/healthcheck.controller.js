f1c9477d3082f2fa1a646edba6514340b1b55a15
Zaryab khalid <zaryabkhalid43@gmail.com>
2024-01-31 19:39:46 +0500

    implement health check controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,6 +2,13 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { expressAsyncHandler } from "../utils/expressAsyncHandler.js";
 
-const healthCheck = expressAsyncHandler(async (req, res) => {});
+const healthCheck = expressAsyncHandler(async (req, res) => {
+  const healthStatus = {
+    healthStatus: "100%",
+    route: `${req.baseUrl}`,
+  };
+
+  return res.status(200).json(new ApiResponse(200, healthStatus, "Working Perfectly..."));
+});
 
 export { healthCheck };
