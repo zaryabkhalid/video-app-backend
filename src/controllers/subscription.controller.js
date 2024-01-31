ac725314fadf0c10acfb570f1853e9c56bceb47f
Zaryab khalid <zaryabkhalid43@gmail.com>
2024-01-31 22:09:12 +0500

    Feature: subscription implemented...

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,23 +1,61 @@
-import mongoose, { isValidObjectId } from "mongoose";
-import { User } from "../models/user.model.js";
-import { Subscription } from "../models/subscription.model.js";
+import { isValidObjectId } from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { expressAsyncHandler } from "../utils/expressAsyncHandler.js";
+import {
+  handleGetUserChannelSubs,
+  handleUserSubscribedChannel,
+  handleToggleSubscription,
+} from "../services/subscription.service.js";
 
 const toggleSubscription = expressAsyncHandler(async (req, res) => {
-  const { channelId } = req.params;
   // TODO: toggle subscription
+  const { channelId } = req.params;
+  const userId = req?.user._id;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "channel ID isn't valid...");
+  }
+
+  const toggleSubscription = await handleToggleSubscription(channelId, userId);
+
+  return res.status(200).json(200, toggleSubscription, "Success");
 });
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = expressAsyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "channel ID is not valid");
+  }
+
+  const userChannelSubscriberList = await handleGetUserChannelSubs(channelId);
+
+  console.log("Subscriber List: ", userChannelSubscriberList);
+
+  if (!userChannelSubscriberList) {
+    throw new ApiError(400, "Subscriber list is Empty");
+  }
+
+  return res.status(200).json(new ApiResponse(200, userChannelSubscriberList, "Subscribers fetched Successfully..."));
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = expressAsyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "subscriber ID is not valid");
+  }
+
+  const subscribedChannels = await handleUserSubscribedChannel(subscriberId);
+
+  if (!subscribedChannels) {
+    throw new ApiError(400, "No Channel Subscribed yet...");
+  }
+
+  return res.status(200).json(new ApiResponse(200, subscribedChannels, "Successfully get subscribed channels"));
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
